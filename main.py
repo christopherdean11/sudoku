0a50d88e5adc6a916437d7abd69505789a4614cf
Chris <christopher.dean11@gmail.com>
2018-02-10 22:46:33 -0500

    refactored to move all solving logic into solver.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 testing out finding missing values in a puzzle row
 
 """
-import header
-import solver
-import sections
+from solver import Solver
+from sections import Sections
 import copy
 
 
@@ -14,78 +13,14 @@ def main():
     :return: n/a
     """
     pz = get_puzzle()
-    solv = solver.Solver(pz)
-
-    s = sections.Sections(pz)
-    row = s.make('row')
-    col = s.make('col')
-    sq = s.make('sq')
+    solv = Solver(pz)
 
     prettyprintpuzzle(copy.deepcopy(pz))
 
-    set_temp_vals(solv, row)
-
-    check_temp_vals(col)
-
-    set_temp_vals(solv, col)
-    check_temp_vals(row)
-
-    set_temp_vals(solv, sq)
-    check_temp_vals(sq)
-
-    check_temp_vals(row)
-    check_temp_vals(col)
-
-    check_temp_vals(col)
-
-    check_temp_vals(row)
-    check_temp_vals(col)
+    solv.solve_puzzle()
 
     prettyprintpuzzle(puzzle=pz)
 
-
-def check_temp_vals(section_type):
-    badvals = []
-    for index in range(0, 9):
-        for subidx, element in enumerate(section_type[index]):
-            if type(element) == list:
-                badvals = []
-                for tempv in element:
-                    if tempv in section_type[index]:
-                        # if index == 3:
-                            # print('found: ' + str(tempv))
-                        badvals.append(tempv)
-                remove_invalid_temps(section_type, index, subidx, badvals)
-
-
-def remove_invalid_temps(section_type, idx, subidx, badvals):
-    s = section_type[idx]
-    for val in badvals:
-        s[subidx].remove(val)
-    section_type[idx] = s
-    is_final_temp(section_type, idx)
-
-
-def is_final_temp(section_type, n):
-    for i, el in enumerate(section_type[n]):
-        if type(el) == list:
-            if len(el) == 1:
-                # workaround while the setter does not support col[x][n] = m syntax
-                s = section_type[n]
-                s[i] = el[0]
-                section_type[n] = s
-
-
-def set_temp_vals(solv, section_type):
-
-    for r in range(0, 9):
-        msgv = solv.find_missing(section_type[r])
-
-        for l in msgv.locations:
-            temps = []
-            for val in msgv.values:
-                temps.append(val)
-            section_type[r][l] = temps
 
 
 def get_puzzle():
--- solver.py
+++ solver.py
@@ -1,146 +1,104 @@
-import solver_helper
 import header
-# from puzzle import Puzzle
+from sections import Sections
 
 
 class Solver:
 
     def __init__(self, puzzle):
-            self.puzzle = puzzle
+        s = Sections(puzzle)
+        self.row = s.make('row')
+        self.col = s.make('col')
+        self.sq = s.make('sq')
 
-    def get_section(self, section: header.Section) -> list:
-        """
-        Get a section slice of the puzzle as a list to do operations on
-        :param section: sectionID (row/col/square, index)
-        :return: flat 1D list with 9 elements
-        """
-        # extract row as list
-        if section.type == 'row':
-            return solver_helper.get_row(self.puzzle, section.idx)
+    def solve_puzzle(self):
+        self.__set_temp_vals(self.row)
+        self.__set_temp_vals(self.col)
+        self.__set_temp_vals(self.sq)
+        i = 0
+        while not self.is_puzzle_complete():
+            self.__check_temp_vals(self.row)
+            self.__check_temp_vals(self.col)
+            self.__check_temp_vals(self.sq)
+            i += 1
+        print('completed in {} rounds\n'.format(i))
 
-        # extract column as list
-        elif section.type == 'col':
-            return solver_helper.get_col(self.puzzle, section.idx)
 
-        # extract square as a list
-        elif section.type == 'square':
-            return solver_helper.get_square(self.puzzle, section.idx)
+    def is_puzzle_complete(self):
 
-    def put_section(self, section: header.Section, flat_list: list):
-        """
-        Put a section back into the puzzle
-        :param section: sectionID (row/col/square, index)
-        :param flat_list: section slice as a list
-        :return: none, written to self.puzzle
-        """
+        r = self.__is_sectiontype_valid(self.row)
+        c = self.__is_sectiontype_valid(self.col)
+        sq = self.__is_sectiontype_valid(self.sq)
 
-        if section.type == 'row':
-            # insert row into puzzle
-            self.puzzle[section.idx] = flat_list
+        if r and c and sq:
+            return True
+        else:
+            return False
 
-        if section.type == 'col':
-            # for each row, update index "idx" with flat_list[row_n]
-            for i, row in enumerate(self.puzzle):
-                row[section.idx] = flat_list[i]
+    def __is_sectiontype_valid(self, section_type) -> bool:
 
-        if section.type == 'square':
-            hstart, hstop, vstart, vstop = solver_helper.get_square_bounds(section.idx)
-            for i, x in enumerate(range(vstart, vstop)):
-                self.puzzle[x][hstart:hstop] = flat_list[i*3:i*3+3]
+        # for all row/col/squares
+        for n in range(0, 9):
 
-    def set_temp_values(self, section: header.Section):
-        """
-        set temporary values in sublist in puzzle
-        :param section: sectionID (row/col/square, index)
-        :return: none, written to self.puzzle
-        """
-        # go to Section
-        # find missing -> Missing(values, locs)
-        # set temps (need to convert flat list back into puzzle)
+            # if not valid, stop checking and return False
+            if not self.__is_section_valid(section_type, n):
+                return False
 
-        # missing_values -> header.Missing(values: list, locations: list)
-        missing_values = self.find_missing(section)
+        # if get to the end, section is valid (haven't returned False yet)
+        return True
 
-        # get the section slice of the puzzle
-        s = self.get_section(section)
+    def __is_section_valid(self, section_type, index) -> bool:
 
-        # for each empty space in the puzzle section
-        for loc in missing_values.locations:
-            # write all possible values in location as a list
-            s[loc] = missing_values.values
+        s = section_type[index]
 
-        # write new list with temps to puzzle
-        self.put_section(section, s)
+        for element in s:
+            if type(element) == list:
+                return False
 
-    def check_values(self):
-        pass
+        if sorted(s) == list(range(1, 10)):
+            return True
+        else:
+            return False
 
-    def fewest_missing(self) -> header.Section:
-        """
-        return a Section type with the least amount of empty spaces
-        :return: Section type with least empties
-        """
-        missing_count = 9
-        best = header.Section('null', 0)
-        # walk through all rows to find least missing values
-        section_types = ['row', 'col', 'square']
-        for sec in section_types:
-            best, missing_count = self._fewest_missing_from_section(best, missing_count, sec)
+    def __set_temp_vals(self, section_type):
 
-        return best
+        for r in range(0, 9):
+            msgv = self.find_missing(section_type[r])
 
-    def _fewest_missing_from_section(self, best: header.Section, missing_count: int, section_type: str):
-        """
-        sub routine for fewest_missing, gets the fewest missing from all possible sections (row, col, sq)
-        :param best: namedtuple (Section) with fewest missing so far in search
-        :param missing_count: number of best case missing items found so far
-        :param section_type: string ('row', 'col', or 'square)
-        :return: new best (or best as passed if no new best found), and missing count
-        """
-        for i in range(9):
-            # make a Section to slice the puzzle by
-            s = header.Section(section_type, i)
-            # get the missing values for the Section
-            temp = self.find_missing(s)
+            for l in msgv.locations:
+                temps = []
+                for val in msgv.values:
+                    temps.append(val)
+                section_type[r][l] = temps
 
-            # if length of missing values is a new low, store this section info and count
-            if len(temp.values) < missing_count:
-                missing_count = len(temp.values)
-                best = header.Section(section_type, i)
+    def __check_temp_vals(self, section_type):
+        badvals = []
+        for index in range(0, 9):
+            for subidx, element in enumerate(section_type[index]):
+                if type(element) == list:
+                    badvals = []
+                    for tempv in element:
+                        if tempv in section_type[index]:
+                            # if index == 3:
+                            # print('found: ' + str(tempv))
+                            badvals.append(tempv)
+                    self.__remove_invalid_temps(section_type, index, subidx, badvals)
 
-        return best, missing_count
 
-    def find_missing_old(self, section_to_search: header.Section) -> header.Missing:
-        """
-        find and return the missing values from a puzzle section as a list
-        eventually implement this as the __len__ method on a puzzle Class for Square, Row, Column
-        :param section_to_search: puzzle section object (square, row, or column)
-        :return namedtuple of type Missing(values, locations)
-        """
-        # Missing = namedtuple('MissingValues', 'values locations')
-        Missing = header.Missing
-        exists = []
-        locs = []
-        full_set = range(1, 10)
-        missing = []
+    def __remove_invalid_temps(self, section_type, idx, subidx, badvals):
+        s = section_type[idx]
+        for val in badvals:
+            s[subidx].remove(val)
+        section_type[idx] = s
+        self.__is_final_temp(section_type, idx)
 
-        elements = self.get_section(section=section_to_search)
-
-        # find non-zero spaces in provided section (row,col,sq)
-        for idx, element in enumerate(elements):
-            if element != 0:
-                # get a list of elements that do exist
-                exists.append(element)
-            elif element == 0:
-                # get a list of the blank locations
-                locs.append(idx)
-
-        # walk through full_set (ints 1-9) and create a list called missing with missing values
-        for value in full_set:
-            if value not in sorted(exists):
-                missing.append(value)
-
-        return Missing(missing, locs)
+    def __is_final_temp(self, section_type, n):
+        for i, el in enumerate(section_type[n]):
+            if type(el) == list:
+                if len(el) == 1:
+                    # workaround while the setter does not support col[x][n] = m syntax
+                    s = section_type[n]
+                    s[i] = el[0]
+                    section_type[n] = s
 
     def find_missing(self, section: list) -> header.Missing:
         """
